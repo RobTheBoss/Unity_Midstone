1f8bdbfb613c1e22480780227623fa2aeb4ffac1
Shedinja321 <86268609+Shedinja321@users.noreply.github.com>
2022-12-05 21:40:24 -0500

    Buttonmanager

---
+++ Midstone_Unity/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
